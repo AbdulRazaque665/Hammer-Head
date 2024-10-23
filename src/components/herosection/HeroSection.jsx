728bf4f4e27763f69b795be2e6f81166a8bf9a9e
AbdulRazaque665 <abdulrazaque.xwave@gmail.com>
2024-10-23 21:47:49 +0500

    initialise the folder

---
+++ src/components/herosection/HeroSection.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import AboutsUs from '../aboutUs/AboutsUs'
+import WhyUs from '../whyUs/WhyUs'
+import Careers from '../careers/Careers'
+import ContactUs from '../contactUs/ContactUs'
+
+const HeroSection = () => {
+  return (
+    <div>
+
+
+     <AboutsUs/>
+     <WhyUs/>
+     <Careers/>
+     <ContactUs/>   
+    </div>
+  )
+}
+
+export default HeroSection
