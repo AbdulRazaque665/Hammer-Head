d67dc5a780fee4b7dbd553677dbb2030e3726013
Iqra <iqrahyderxwave@gmail.com>
2024-10-26 10:57:31 +0500

    About and contact pages completed

--- src/components/contactUs/ContactUs.jsx
+++ src/components/contactUs/ContactUs.jsx
@@ -2,83 +2,114 @@ import { Box, TextField, Typography } from "@mui/material";
 import React from "react";
 import Grid from "@mui/material/Grid";
 import ContactImg from "../../asset/contact-background.png";
+import AskedQuestions from "./AskedQuestions";
 
 const ContactUs = () => {
   return (
     <div>
       <div
         style={{
-          height: "85vh",
+          minHeight: "100%",
           width: "100%",
           backgroundSize: "cover",
           backgroundRepeat: "no-repeat",
           backgroundImage: `url(${ContactImg})`,
         }}
       >
-        <Grid container className="container bg-transparent p-sm-5">
-          <Grid item xs={12} lg={6}>
-            <Typography variant="h4" className="text-center fw-bold pt-3">
-              Tell us About Your Construction Project!
-            </Typography>
-            <Box className="border border-info border-3 mt-4 mt-sm-4 p-4 ">
-              <Grid container spacing={3}>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    fullWidth
-                    id="outlined-basic"
-                    label="First Name"
-                    variant="outlined"
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    fullWidth
-                    id="outlined-basic"
-                    label="Last Name"
-                    variant="outlined"
-                  />
-                </Grid>
-              </Grid>
-              #
-              <Grid container spacing={3}>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    fullWidth
-                    id="outlined-basic"
-                    label="Your Email"
-                    variant="outlined"
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    fullWidth
-                    id="outlined-basic"
-                    label="Phone"
-                    variant="outlined"
-                  />
-                </Grid>
-              </Grid>
-              #
-              <Grid container spacing={3}>
-                <Grid item xs={12}>
-                  <TextField
-                    fullWidth
-                    id="outlined-basic"
-                    label="Your Email"
-                    variant="outlined"
-                  />
-                </Grid>
-              </Grid>
-              <button
-                className="border-0 text-white text-capitalize fw-semibold py-2 px-4 mt-4"
-                style={{ backgroundColor: "#1C64EA" }}
+        <div
+          style={{
+            backgroundColor: "rgba(0, 0, 0, 0.60)",
+            minHeight: "100%",
+            width: "100%",
+          }}
+        >
+          <Grid container className="container p-sm-5 py-3">
+            <Grid item xs={12} lg={6}>
+              <Typography
+                variant="h4"
+                className="text-center text-white fw-bold pt-4"
               >
-                Send
-              </button>
-            </Box>
+                Tell us About Your Construction Project!
+              </Typography>
+              <Box className="mt-md-4">
+                <Grid container spacing={3} className="my-2">
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      fullWidth
+                      className="bg-white rounded-3"
+                      id="outlined-basic"
+                      placeholder="First Name"
+                      variant="outlined"
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      className="bg-white rounded-3"
+                      fullWidth
+                      id="outlined-basic"
+                      placeholder="Last Name"
+                      variant="outlined"
+                    />
+                  </Grid>
+                </Grid>
+
+                <Grid container spacing={3} className="my-2">
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      fullWidth
+                      className="bg-white rounded-3"
+                      id="outlined-basic"
+                      placeholder="Your Email"
+                      variant="outlined"
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      fullWidth
+                      className="bg-white rounded-3"
+                      id="outlined-basic"
+                      placeholder="Phone"
+                      variant="outlined"
+                    />
+                  </Grid>
+                </Grid>
+
+                <Grid container spacing={3} className="my-2">
+                  <Grid item xs={12}>
+                    <TextField
+                      fullWidth
+                      className="bg-white rounded-3"
+                      id="outlined-basic"
+                      placeholder="Service you need"
+                      variant="outlined"
+                    />
+                  </Grid>
+                </Grid>
+                <Grid container spacing={3} className="my-2">
+                  <Grid item xs={12}>
+                    <TextField
+                      id="outlined-multiline-static"
+                      fullWidth
+                      className="bg-white rounded-3"
+                      placeholder="Message"
+                      multiline
+                      rows={4}
+                    />
+                  </Grid>
+                </Grid>
+
+                <button
+                  className="border-0 text-white text-capitalize rounded-3 fs-5 fw-semibold py-1 px-4 mt-4"
+                  style={{ backgroundColor: "#1C64EA" }}
+                >
+                  Send
+                </button>
+              </Box>
+            </Grid>
           </Grid>
-        </Grid>
+        </div>
       </div>
+      <AskedQuestions/>
     </div>
   );
 };
