2d3c649bc5b6a076558bcbca4d69f999783bead4
AbdulRazaque665 <abdulrazaque.xwave@gmail.com>
2024-10-27 16:59:04 +0500

    work on home page

--- src/components/herosection/HeroSection.jsx
+++ src/components/herosection/HeroSection.jsx
@@ -7,6 +7,10 @@ import HeroImg from "../../asset/buildong.png"
 import Typography from '@mui/material/Typography';
 import "./herosection.css"
 import AlertDialog from './dialog/Dialog'
+import Teamlogo from "../../asset/1.png"
+import Clientlogo from "../../asset/2.png"
+import Materiallogo from "../../asset/3.png"
+import { Divider } from '@mui/material'
 
 
 
@@ -32,6 +36,25 @@ const HeroSection = () => {
       <div className="col-sm-12 col-md-6 col-lg-6 text-end d-lg-block d-md-block d-none hero-ani-right">
        <img src={HeroImg} className="img-fluid w-75"/>
       </div>
+      </div>
+      <div className='row mx-lg-4 mx-md-4 mx-sm-none align-items-center gy-4 ' style={{marginTop:"80px"}}>
+        <div className='col-lg-4 border-end border-2 border-black'>
+          <img src={Teamlogo} alt="" />
+          <Typography variant="h6" color="initial">Team Work</Typography>
+          <Typography variant="h6" color="initial">Our experienced team values quality, safety, and innovation in every project.</Typography>
+        </div>
+
+
+        <div className='col-lg-4 border-end border-2 border-black'>
+          <img src={Clientlogo} alt="" />
+          <Typography variant="h6" color="initial">Client Satisfaction</Typography>
+          <Typography variant="h6" color="initial">Committed to sustainability and client satisfaction, we strive to build lasting relationships.</Typography>
+        </div>
+        <div className='col-lg-4'>
+          <img src={Materiallogo} alt="" />
+          <Typography variant="h6" color="initial">Solid material</Typography>
+          <Typography variant="h6" color="initial">Explore our wide variety of categories with over 50 unique items and materials to choose from!</Typography>
+        </div>
       </div>
       </div>
 
