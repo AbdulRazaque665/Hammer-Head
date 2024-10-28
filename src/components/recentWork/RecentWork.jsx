2e48b17eae2aaba261ae718a0933a2f1e9b5aec9
AbdulRazaque665 <abdulrazaque.xwave@gmail.com>
2024-10-28 17:14:17 +0500

    correct the margin and padding

--- src/components/recentWork/RecentWork.jsx
+++ src/components/recentWork/RecentWork.jsx
@@ -14,7 +14,7 @@ import Img9 from "../../asset/image-8.png"
 
 const RecentWork = () => {
   return (
-    <Box className="text-center" sx={{marginTop:"70px",paddingBottom:"150px"}}>
+    <Box className="text-center" sx={{marginTop:"40px",paddingBottom:"150px"}}>
       <Typography className='text-center fw-semibold logo-text' variant="h3" color="initial">Recent Work</Typography>
       <Box className="container">
       <Typography className='text-start pb-5 pt-3' variant="h5" color="initial">At hammerhead, we take pride in our diverse range of construction projects that reflect our commitment to quality and innovation. Below are some of our most recent works that showcase our expertise and dedication to excellence.</Typography>
